88b6ef36168f5f3b9c27b418c15fc1cf9af7267b
Majed Amin <majedamin974@gmail.com>
2019-06-03 12:21:33 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,13 +1,13 @@
-function forLoop(array) {
-    for (let i = 0; i < 25; i++) {
-        if (i === 1) {
-            array[i] = "I am 1 strange loop.";
-        } else {
-            array[i] = "I am " + i + " strange loop.";
-        }
+function forLoop(array){
+  for (let i = 0; i < 25; i++){
 
+     if(i === 1) {
+      array[i]= "I am 1 strange loop." ;
+    } else {
+      array[i]= "I am " + i + " strange loops." ;
     }
-    return array;
+  }
+  return array;
 }
 
 function whileLoop(n) {
